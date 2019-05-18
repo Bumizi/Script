887cffd9022125489884143e6231b1bad4f8018b
Bumizi <43131635+Bumizi@users.noreply.github.com>
2019-05-19 02:46:09 +0900

    텀프로젝트 2차
    -드디어 검색방법 구현!
    -지역별,기간별,분류별 버튼 클릭에 따라 위젯 출력 구현
    -콤보박스 활용

--- Project/TkinterGUI.py
+++ Project/TkinterGUI.py
@@ -1,55 +1,74 @@
 from tkinter import *
+from tkinter import ttk
 import tkinter.messagebox
 import random
 
 g_Tk = Tk()
 g_Tk.geometry("800x600+750+200")
 g_Tk.title("공연 / 전시 정보 조회 서비스")
-frame = Frame(g_Tk)
-frame.pack(fill=BOTH, ipadx=0, ipady=50, padx=0, pady=0, side=TOP)
 DataList = []
-SearchMode = 0
+SearchMode = 3
 
+SearchLabel = Label(g_Tk, text="지역별 검색")
+OptionLabel1 = Label(g_Tk, text="시/도")
+OptionLabel2 = Label(g_Tk, text="구/군")
+MonthLabel1 = Label(g_Tk, text="월")
+MonthLabel2 = Label(g_Tk, text="월")
+DayLabel1 = Label(g_Tk, text="일")
+DayLabel2 = Label(g_Tk, text="일")
+InputLabel1 = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
+InputLabel2 = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
+RealmComboBox = ttk.Combobox(g_Tk, width=15)
+sMonthComboBox = ttk.Combobox(g_Tk, width=3, value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"])
+
+sDayComboBox = ttk.Combobox(g_Tk, width=3, value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+                                                 "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+                                                 "25", "26", "27", "28", "29", "30", "31"])
+eMonthComboBox = ttk.Combobox(g_Tk, width=3, value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"])
+
+eDayComboBox = ttk.Combobox(g_Tk, width=3, value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+                                                 "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24",
+                                                 "25", "26", "27", "28", "29", "30", "31"])
+
+# 타이틀 표시
 def InitTopText():
     MainText = Label(g_Tk, text="[공연 / 전시 정보검색 서비스]")
-    MainText.pack()
     MainText.place(x=330)
 
+# 기능별 버튼 생성
 def SearchCategoryButton():
-
-    AreaSearch = Button(frame, text="지역별", command=InitSearchByArea)
-    AreaSearch.pack()
+    AreaSearch = Button(g_Tk, text="지역별", command=InitSearchByArea)
     AreaSearch.place(x=10, y=10)
-    PeriodSearch = Button(frame, text="기간별", command=InitSearchByPeriod)
-    PeriodSearch.pack()
+    PeriodSearch = Button(g_Tk, text="기간별", command=InitSearchByPeriod)
     PeriodSearch.place(x=70, y=10)
-    RealmSearch = Button(frame, text="분야별", command=SearchButtonAction)
-    RealmSearch.pack()
+    RealmSearch = Button(g_Tk, text="분야별", command=InitSearchByRealm)
     RealmSearch.place(x=130, y=10)
 
+# 지역별 버튼 클릭 시 UI
 def InitSearchByArea():
+    global SearchMode
+    SearchMode = 0
 
-    area = Label(g_Tk, text="지역별 검색")
-    area.pack()
-    area.place(x=130, y=50)
+    sMonthComboBox.place_forget()
+    sDayComboBox.place_forget()
+    eMonthComboBox.place_forget()
+    eDayComboBox.place_forget()
+    MonthLabel1.place_forget()
+    MonthLabel2.place_forget()
+    DayLabel1.place_forget()
+    DayLabel2.place_forget()
+    RealmComboBox.place_forget()
 
-    sido = Label(g_Tk, text="시 / 도")
-    sido.pack()
-    sido.place(x=30, y=100)
+    SearchButton.place(x=320, y=125)
 
-    gugun = Label(g_Tk, text="구 / 군")
-    gugun.pack()
-    gugun.place(x=30, y=150)
-
-    global sidoInputLabel
-    sidoInputLabel = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
-    sidoInputLabel.pack()
-    sidoInputLabel.place(x=80, y=95)
-
-    global gugunInputLabel
-    gugunInputLabel = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
-    gugunInputLabel.pack()
-    gugunInputLabel.place(x=80, y=145)
+    SearchLabel.config(text="지역별 검색")
+    SearchLabel.place(x=130, y=50)
+    OptionLabel1.config(text="시/도")
+    OptionLabel1.place(x=30, y=100)
+    OptionLabel2.config(text="구/군")
+    OptionLabel2.place(x=30, y=150)
+    InputLabel1.place(x=80, y=95)
+    InputLabel2.place(x=80, y=145)
 
     #sidoScrollbar = Scrollbar(g_Tk)
     #sidoScrollbar.pack(fill="y")
@@ -59,37 +78,56 @@ def InitSearchByArea():
     #sidoListBox.place(x=100, y=90)
     #sidoScrollbar.config(command=sidoListBox.yview)
 
+# 기간별 버튼 클릭 시 UI
 def InitSearchByPeriod():
+    global SearchMode
+    SearchMode = 1
 
-    period = Label(g_Tk, text="기간별 검색")
-    period.pack()
-    period.place(x=130, y=50)
+    InputLabel1.place_forget()
+    InputLabel2.place_forget()
+    RealmComboBox.place_forget()
 
-    start = Label(g_Tk, text="시작")
-    start.pack()
-    start.place(x=10, y=100)
+    SearchButton.place(x=320, y=125)
 
-    end = Label(g_Tk, text="종료")
-    end.pack()
-    end.place(x=10, y=150)
+    SearchLabel.config(text="기간별 검색")
+    SearchLabel.place(x=130, y=50)
+    OptionLabel1.config(text="시작일")
+    OptionLabel1.place(x=30, y=100)
+    OptionLabel2.config(text="종료일")
+    OptionLabel2.place(x=30, y=150)
+    MonthLabel1.place(x=150, y=100)
+    DayLabel1.place(x=250, y=100)
+    MonthLabel2.place(x=150, y=150)
+    DayLabel2.place(x=250, y=150)
+    sMonthComboBox.place(x=100, y=100)
+    sDayComboBox.place(x=200, y=100)
+    eMonthComboBox.place(x=100, y=150)
+    eDayComboBox.place(x=200, y=150)
 
-    sido = Label(g_Tk, text="시 / 도")
-    sido.pack()
-    sido.place(x=30, y=100)
+# 분야별 버튼 클릭 시 UI
+def InitSearchByRealm():
+    global SearchMode
+    SearchMode = 2
 
-    gugun = Label(g_Tk, text="구 / 군")
-    gugun.pack()
-    gugun.place(x=30, y=150)
+    SearchButton.place(x=320, y=125)
 
-    global sidoInputLabel
-    sidoInputLabel = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
-    sidoInputLabel.pack()
-    sidoInputLabel.place(x=80, y=95)
+    sMonthComboBox.place_forget()
+    sDayComboBox.place_forget()
+    eMonthComboBox.place_forget()
+    eDayComboBox.place_forget()
+    MonthLabel1.place_forget()
+    MonthLabel2.place_forget()
+    DayLabel1.place_forget()
+    DayLabel2.place_forget()
+    OptionLabel2.place_forget()
+    InputLabel1.place_forget()
+    InputLabel2.place_forget()
 
-    global gugunInputLabel
-    gugunInputLabel = Entry(g_Tk, width=26, borderwidth=6, relief='ridge')
-    gugunInputLabel.pack()
-    gugunInputLabel.place(x=80, y=145)
+    SearchLabel.config(text="분야별 검색")
+    SearchLabel.place(x=130, y=50)
+    OptionLabel1.config(text="분야")
+    OptionLabel1.place(x=30, y=100)
+    RealmComboBox.place(x=80, y=100)
 
 def InitSearchListBox():
     global SearchListBox
@@ -105,16 +143,19 @@ def InitSearchListBox():
     SearchListBox.place(x=500, y=50)
     ListBoxScrollbar.config(command=SearchListBox.yview)
 
+#일단 안씀
 def InitInputLabel():
     global InputLabel
     InputLabel = Entry(g_Tk, width = 26, borderwidth = 12, relief = 'ridge')
     InputLabel.pack()
-    InputLabel.place(x=1000, y=105)
+    InputLabel.place(x=500, y=105)
 
+#일단 안씀
 def InitSearchButton():
-    SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
-    SearchButton.pack()
-    SearchButton.place(x=320, y=125)
+    #SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
+    #SearchButton.pack()
+    #SearchButton.place(x=320, y=125)
+    pass
 
 def SearchButtonAction():
     global SearchListBox, SearchMode
@@ -201,14 +242,14 @@ def InitRenderText():
     RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
     RenderText.configure(state='disabled')
 
-SearchCategoryButton()
-#InitSearchByArea()
-#InitSearchByPeriod()
 
+SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
+
+SearchCategoryButton()
 InitTopText()
 InitSearchListBox()
-InitInputLabel()
-InitSearchButton()
+#InitInputLabel()
+#InitSearchButton()
 InitRenderText()
 #InitSendEmailButton()
 #InitSortListBox()
