8730ddb1423f2776cbf4c2a1b9f6952d2c5f73b0
Bumizi <43131635+Bumizi@users.noreply.github.com>
2019-05-26 20:50:11 +0900

    텀프로젝트 7차
    검색 기능 및 리스트 출력 구현
    일련번호를 통한 상세정보 검색 기능 및 리스트 출력 구현

--- Project/TkinterGUI.py
+++ Project/TkinterGUI.py
@@ -16,7 +16,7 @@ OptionLabel1 = Label(g_Tk, font=TempFont2, text="시/도")
 OptionLabel2 = Label(g_Tk, font=TempFont2, text="구/군")
 InputLabel1 = Entry(g_Tk, width=18, borderwidth=6, relief='ridge')
 InputLabel2 = Entry(g_Tk, width=18, borderwidth=6, relief='ridge')
-
+InputLabel3 = Entry(g_Tk, width=18, borderwidth=6, relief='ridge')
 RealmComboBox = ttk.Combobox(g_Tk, width=15, value=["연극", "음악", "무용", "미술", "건축", "영상", "문학", "문화정책",
                                                     "축제문화공간", "기타"])
 
@@ -27,10 +27,9 @@ def InitTopText():
     MainText.place(x=300, y=10)
     SpecificSearchLabel = Label(g_Tk, font=TempFont2, text="일련번호")
     SpecificSearchLabel.place(x=420, y=70)
-    InputLabel3 = Entry(g_Tk, width=18, borderwidth=6, relief='ridge')
     InputLabel3.place(x=490, y=65)
 
-# 기능별 버튼 생성
+# 기능별 버튼 출력
 def SearchCategoryButton():
     AreaSearch = Button(g_Tk, font=TempFont1, text="지역별", command=InitSearchByArea)
     AreaSearch.place(x=10, y=10)
@@ -39,7 +38,7 @@ def SearchCategoryButton():
     RealmSearch = Button(g_Tk, font=TempFont1, text="분야별", command=InitSearchByRealm)
     RealmSearch.place(x=190, y=10)
 
-# 지역별 버튼 클릭 시 UI
+# 지역별 버튼 클릭
 def InitSearchByArea():
     global SearchMode
     SearchMode = 0
@@ -52,9 +51,11 @@ def InitSearchByArea():
     OptionLabel2.config(text="구/군")
     OptionLabel2.place(x=30, y=150)
     InputLabel1.place(x=80, y=95)
+    InputLabel1.delete(0, END)
     InputLabel2.place(x=80, y=145)
+    InputLabel2.delete(0, END)
 
-# 기간별 버튼 클릭 시 UI
+# 기간별 버튼 클릭
 def InitSearchByPeriod():
     global SearchMode
     SearchMode = 1
@@ -67,9 +68,11 @@ def InitSearchByPeriod():
     OptionLabel2.config(text="종료 연월일")
     OptionLabel2.place(x=30, y=150)
     InputLabel1.place(x=110, y=95)
+    InputLabel1.delete(0, END)
     InputLabel2.place(x=110, y=145)
+    InputLabel2.delete(0, END)
 
-# 분야별 버튼 클릭 시 UI
+# 분야별 버튼 클릭
 def InitSearchByRealm():
     global SearchMode
     SearchMode = 2
@@ -83,6 +86,7 @@ def InitSearchByRealm():
     OptionLabel1.place(x=30, y=100)
     RealmComboBox.place(x=80, y=100)
 
+# 안씀
 def InitSearchListBox():
     global SearchListBox
     ListBoxScrollbar = Scrollbar(g_Tk)
@@ -97,26 +101,14 @@ def InitSearchListBox():
     SearchListBox.place(x=500, y=50)
     ListBoxScrollbar.config(command=SearchListBox.yview)
 
-#일단 안씀
-def InitInputLabel():
-    global InputLabel
-    InputLabel = Entry(g_Tk, width = 26, borderwidth = 12, relief = 'ridge')
-    InputLabel.pack()
-    InputLabel.place(x=500, y=105)
-
-#일단 안씀
+# 검색 버튼 출력
 def InitSearchButton():
-    SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
-    SearchButton.pack()
+    SearchButton = Button(g_Tk, font=TempFont2, text="검색",  command=SearchButtonAction)
     SearchButton.place(x=320, y=125)
-    pass
+    SpecificSearchButton = Button(g_Tk, font=TempFont2, text="검색", command=SpecificSearchButtonAction)
+    SpecificSearchButton.place(x=650, y=65)
 
-def InitSearchButton2():
-    SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
-    SearchButton.pack()
-    SearchButton.place(x=650, y=65)
-
-# 검색 버튼 클릭 시
+# 검색 버튼 클릭
 def SearchButtonAction():
     global SearchListBox, SearchMode
     RenderText.configure(state='normal')
@@ -129,7 +121,15 @@ def SearchButtonAction():
         DisplayPerforList(getApi_SearchByRealm(RealmComboBox.get(), 999))
     RenderText.configure(state='disabled')
 
+# 상세 검색 버튼 클릭
+def SpecificSearchButtonAction():
+    global SearchListBox, SearchMode
+    RenderText2.configure(state='normal')
+    RenderText2.delete(0.0, END)
+    DisplaySpecificInfo(getApi_SearchBySeq(InputLabel3.get()))
+    RenderText.configure(state='disabled')
 
+# 공연전시 리스트 출력
 def DisplayPerforList(DataList):
     for i in range(len(DataList)):
         RenderText.insert(INSERT, "[")
@@ -157,7 +157,67 @@ def DisplayPerforList(DataList):
         RenderText.insert(INSERT, DataList[i][6])
         RenderText.insert(INSERT, "\n\n")
 
+# 공연전시 세부정보 출력
+def DisplaySpecificInfo(DataList):
+    for i in range(len(DataList)):
+        RenderText2.insert(INSERT, "[일련번호]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][0])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[제목]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][1])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[시작일]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][2])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[마감일]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][3])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[장소]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][4])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[분류]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][5])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[지역]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][6])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[부제]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][7])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[가격]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][8])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[주내용]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][9])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[소내용]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][10])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[URL 주소]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][11])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[연락처]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][12])
+        RenderText2.insert(INSERT, "\n\n")
+        RenderText2.insert(INSERT, "[상세 주소]")
+        RenderText2.insert(INSERT, "\n")
+        RenderText2.insert(INSERT, DataList[i][15])
+        RenderText2.insert(INSERT, "\n\n")
 
+# 안씀
 def SearchLibrary():
     import http.client
     from xml.dom.minidom import parse, parseString
@@ -207,7 +267,7 @@ def SearchLibrary():
                 RenderText.insert(INSERT, DataList[i][2])
                 RenderText.insert(INSERT, "\n\n")
 
-
+# 공연전시 리스트 창
 def InitRenderText():
     global RenderText
     RenderTextScrollbar = Scrollbar(g_Tk)
@@ -218,13 +278,14 @@ def InitRenderText():
     RenderTextScrollbar.place(x=380, y=190)
     RenderText.configure(state='disabled')
 
-def InitRenderText2():
+# 공연전시 세부정보 창
+def InitSpecificRenderText():
     global RenderText2
     RenderTextScrollbar2 = Scrollbar(g_Tk)
     RenderText2 = Text(g_Tk, width=49, height=36, borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar2.set)
     RenderText2.pack()
     RenderText2.place(x=410, y=100)
-    RenderTextScrollbar2.config(command=RenderText.yview)
+    RenderTextScrollbar2.config(command=RenderText2.yview)
     RenderTextScrollbar2.place(x=780, y=100)
     RenderText.configure(state='disabled')
 
@@ -233,14 +294,11 @@ SearchButton = Button(g_Tk, text="검색",  command=SearchButtonAction)
 
 SearchCategoryButton()
 InitTopText()
-#InitSearchListBox()
-
-InitInputLabel()
-#InitSearchButton()
-InitSearchButton2()
+InitSearchByArea()
+InitSearchButton()
 
 InitRenderText()
-InitRenderText2()
+InitSpecificRenderText()
 #InitSendEmailButton()
 #InitSortListBox()
 #InitSortButton()
